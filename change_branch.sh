06b6d64a097743c0dccd3769b79f33c68c55d675
Pratheek <pratheek.bht@gmail.com>
2022-03-09 14:23:19 +0530

    Changed the scripts to match exact patterns.

--- change_branch.sh
+++ change_branch.sh
@@ -5,27 +5,38 @@ gitBranch=$(git branch)
 if [[ $# -eq 0 ]]; then 
 	echo "No arguments passed."
 	exit 1
-fi
+elif [[ $# -eq 1 ]]; then
+	branches=$(echo "$gitBranch" | grep -i "$1")
+	noOfBranches=$(echo "$branches" | grep -vc "^$" | tr -d ' ')
 
-branches=$(echo "$gitBranch" | grep -i "$1")
-noOfBranches=$(echo "$branches" | grep -vc "^$" | tr -d ' ')
-
-if [ "$noOfBranches" -eq 0 ]; then
-	echo "No branches found."
-	exit 2
-elif [ "$noOfBranches" -eq 1 ]; then
-	branch=$(echo "$branches" | tr -d '* ')
-	echo "Branch found: $branch"
-	echo "Checking out to branch..."
-	git checkout "$branch"
-else
-	echo "More than 1 branch found. Please add a more specific search term."
-	
-	read -rp "Do you wish to see all matching branches? [y/n]" yesOrNo
-	if [[ "$yesOrNo" =~ ^([Yy])$ ]]; then
-		echo "Branches found:" 
-		echo "$branches"
+	if [ "$noOfBranches" -eq 0 ]; then
+		echo "No branches found."
+		exit 2
+	elif [ "$noOfBranches" -eq 1 ]; then
+		branch=$(echo "$branches" | tr -d '* ')
+		echo "Branch found: $branch"
+		echo "Checking out to branch..."
+		git checkout "$branch"
 	else
-		exit 3;
+		echo "More than 1 branch found. Please add a more specific search term or enter 'e' as an argument after the branch name."
+		
+		read -rp "Do you wish to see all matching branches? [y/n]" yesOrNo
+		if [[ "$yesOrNo" =~ ^([Yy])$ ]]; then
+			echo "Branches found:" 
+			echo "$branches"
+		else
+			exit 3;
+		fi
+	fi 
+elif [[ $# -eq 2 ]] && [[ $2 == 'e' ]]; then
+	rawBranchOutput=$(echo "$gitBranch" | grep -E "(^|\s)$1($|\s)")
+	branch=$(echo "$rawBranchOutput" | tr -d '* ')
+
+	if [ "$branch" ]; then
+		echo "Branch found: $branch"
+		echo "Checking out to branch..."
+		git checkout "$branch"
+	else
+		echo "No branches found."
 	fi
-fi 
+fi
--- pull_from_remote.sh
+++ pull_from_remote.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
-
 gitBranch=$(git branch)
 
-if [[ $# -eq 0 ]]; then
-    echo "No arguments passed."
-    exit 1
-fi
+if [[ $# -eq 0 ]]; then 
+	echo "No arguments passed."
+	exit 1
+elif [[ $# -eq 1 ]]; then
+	branches=$(echo "$gitBranch" | grep -i "$1")
+	noOfBranches=$(echo "$branches" | grep -vc "^$" | tr -d ' ')
 
-branches=$(echo "$gitBranch" | grep -i "$1")
-noOfBranches=$(echo "$branches" | grep -vc "^$" | tr -d ' ')
+	if [ "$noOfBranches" -eq 0 ]; then
+		echo "No branches found."
+		exit 2
+	elif [ "$noOfBranches" -eq 1 ]; then
+		branch=$(echo "$branches" | tr -d '* ')
+		echo "Branch found: $branch"
+		echo "Pulling from origin..."
+        git pull origin "$branch"
+	else
+		echo "More than 1 branch found. Please add a more specific search term or enter 'e' as an argument after the branch name."
+		
+		read -rp "Do you wish to see all matching branches? [y/n]" yesOrNo
+		if [[ "$yesOrNo" =~ ^([Yy])$ ]]; then
+			echo "Branches found:" 
+			echo "$branches"
+		else
+			exit 3;
+		fi
+	fi 
+elif [[ $# -eq 2 ]] && [[ $2 == 'e' ]]; then
+	rawBranchOutput=$(echo "$gitBranch" | grep -E "(^|\s)$1($|\s)")
+	branch=$(echo "$rawBranchOutput" | tr -d '* ')
 
-if [ "$noOfBranches" -eq 0 ]; then
-    echo "No branches found."
-    exit 2
-elif [ "$noOfBranches" -eq 1 ]; then
-    branch=$(echo "$branches" | tr -d '* ') 
-    echo "Branch found: $branch"   
-    echo "Pulling from origin..."
-    git pull origin "$branch"
-else
-    echo "More than 1 branch found. Use a more specific search term."
+	if [ "$branch" ]; then
+		echo "Branch found: $branch"
+		echo "Pulling from origin..."
+        git pull origin "$branch"
+	else
+		echo "No branches found."
+	fi
 fi
